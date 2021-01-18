b16a1e7a3545ddce168e3fb644572f85f0abe604
Vitor Henckel <vitor@henckel.com.br>
2021-01-18 12:52:54 -0300

    chore: add model to manipulate Ask table

---
+++ database/Ask.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize")
+const connection = require("./database")
+
+const Ask = connection.define('ask', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+})
+Ask.sync({
+    force: false
+  })
+  .then(() => {})
+
+module.exports = Ask
