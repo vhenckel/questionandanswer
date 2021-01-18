e475579b2a186d35c21353d9c08e0fdde9d49fdf
Vitor Henckel <vitor@henckel.com.br>
2021-01-18 12:54:14 -0300

    refactor: add database connection and model to save data

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+
+const AskModel = require("./database/Ask")
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log('DB success')
+  })
+  .catch((error) => {
+    console.log('DbError: ', error)
+  })
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   res.render("index")
@@ -13,7 +30,17 @@ app.get("/ask", (req, res) => {
 })
 
 app.post("/saveask", (req, res) => {
-  res.send("Ask saved...")
+  const {
+    title,
+    description
+  } = req.body
+
+  AskModel.create({
+    title,
+    description
+  }).then(() => {
+    res.redirect("/")
+  })
 })
 
 app.listen(8080, () => {
