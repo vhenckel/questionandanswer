bfb4a22405e9c4d1d04d60b6f62c5ee60c9c8fd8
Vitor Henckel <vitor@henckel.com.br>
2021-01-21 16:49:46 -0300

    chore: add model to manipulate Answer table

---
+++ database/Answer.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize")
+const connection = require("./database")
+
+const Answer = connection.define('answer', {
+  body: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  askId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+Answer.sync({
+  force: false
+})
+
+module.exports = Answer
